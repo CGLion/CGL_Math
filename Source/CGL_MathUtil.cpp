bb1e7d4ca26a65fec99966144f06c07cc30e5278
CG-Lion Studio <OdedErell@gmail.com>
2021-08-08 22:46:24 +0300

    Added static utility functions
    Summing list of numbers, normalizing list of numbers

--- Source/CGL_MathUtil.cpp
+++ Source/CGL_MathUtil.cpp
@@ -1,5 +1,6 @@
 #include "CGL_MathUtil.h"
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
@@ -7,6 +8,35 @@ namespace CGL_Math
 {
 	bool CGL_MathUtil::CheckInt(double _Number)
 	{
-		return (_Number - floor(_Number) == 0.0) ? true : false;
+		return (_Number - std::floor(_Number) == 0.0) ? true : false;
+	}
+
+	double CGL_MathUtil::ListSum(std::vector<double>& list)
+	{
+		double sum = 0.0;
+		for (auto& n : list)
+		{
+			sum += n;
+		}
+		return sum;
+	}
+
+	double CGL_MathUtil::ListSumAbs(std::vector<double>& list)
+	{
+		double sum = 0.0;
+		for (auto& n : list)
+		{
+			sum += std::abs(n);
+		}
+		return sum;
+	}
+	
+	void CGL_MathUtil::ListValsToWeights(std::vector<double>& list)
+	{
+		double sum = CGL_MathUtil::ListSumAbs(list);
+		for (auto& n : list)
+		{
+			 n = n / sum;
+		}
 	}
 }
--- Source/CGL_MathUtil.h
+++ Source/CGL_MathUtil.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 namespace CGL_Math
 {
 	class CGL_MathUtil
@@ -9,6 +11,22 @@ namespace CGL_Math
 		* Returns true if the supplied number is an integer
 		*/
 		static bool CheckInt(double _Number);
+
+		/**
+		* Returns the sum of the list elements: 
+		*/
+		static double ListSum(std::vector<double>& list);
+
+		/**
+		* Returns the sum of the list elements absolute values:
+		*/
+		static double ListSumAbs(std::vector<double>& list);
+
+		/**
+		* Converts the list of values to a normalized weight list (sum of 1.0):
+		* Uses absolute values.
+		*/
+		static void ListValsToWeights(std::vector<double>& list);
 	};
 }
 
